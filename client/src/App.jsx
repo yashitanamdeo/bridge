4fefd0e8f8e15399e1a72d4f8a0f6d99aec74d35
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-04-27 00:11:32 +0530

    client side initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelListContainer, ChannelContainer } from './components';
+
+const apiKey = 'h8angzera3mz';
+
+const client = StreamChat.getInstance(apiKey);
+
+const App = () => {
+  return (
+    <div className="app__wrapper">
+        <Chat client={client} theme="team light">
+            <ChannelListContainer
+
+            />
+            <ChannelContainer
+
+            />
+        </Chat>
+    </div>
+  );
+}
+
+export default App;
