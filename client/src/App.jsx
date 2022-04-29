916a87f7f0858c634e2202b72ca526a93a081f1a
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-04-30 01:46:10 +0530

    assets files added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,25 @@
-import React from 'react';
-import { StreamChat } from 'stream-chat';
-import { Chat } from 'stream-chat-react';
-import Cookies from 'universal-cookie';
+import React from "react";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
 
-import { ChannelListContainer, ChannelContainer } from './components';
+import { ChannelListContainer, ChannelContainer } from "./components";
 
-import './App.css';
+import "./App.css";
 
-const apiKey = 'h8angzera3mz';
+const apiKey = "h8angzera3mz";
 
 const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
     <div className="app__wrapper">
-        <Chat client={client} theme="team light">
-            <ChannelListContainer
-
-            />
-            <ChannelContainer
-
-            />
-        </Chat>
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
     </div>
   );
-}
+};
 
 export default App;
--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+// SideBar
+import React from "react";
+import { ChannelList, useChatContext } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
+import HospitalIcon from "../assests/hospital.png";
+
+const SideBar = () => (
+  <div className="channel-list__sidebar">
+    <div className="channel-list__sidebar__icon1">
+      <div className="icon1__inner">
+        <img src={HospitalIcon} alt="Hospital" width="30" />
+      </div>
+    </div>
+  </div>
+);
 
 const ChannelListContainer = () => {
-  return (
-    <div>ChannelListContainer</div>
-  );
-}
+  return <div>ChannelListContainer</div>;
+};
 
 export default ChannelListContainer;
