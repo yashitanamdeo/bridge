22ee769f23fc56d7903ec38fb828c386ceab6873
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-04-30 01:51:18 +0530

    navigation bar initial setup

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -5,6 +5,7 @@ import Cookies from "universal-cookie";
 
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
 import HospitalIcon from "../assets/hospital.png";
+import LogoutIcon from "../assets/logout.png";
 
 const SideBar = () => (
   <div className="channel-list__sidebar">
@@ -13,11 +14,21 @@ const SideBar = () => (
         <img src={HospitalIcon} alt="Hospital" width="30" />
       </div>
     </div>
+    <div className="channel-list__sidebar__icon2">
+      <div className="icon1__inner">
+        <img src={LogoutIcon} alt="Logout" width="30" />
+      </div>
+    </div>
   </div>
 );
 
 const ChannelListContainer = () => {
-  return <div>ChannelListContainer</div>;
+  return (
+    <>
+      {" "}
+      <SideBar />{" "}
+    </>
+  );
 };
 
 export default ChannelListContainer;
