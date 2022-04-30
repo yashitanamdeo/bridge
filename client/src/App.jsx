6b0dfcdced27538afa191d1a0736ca9f22fef722
Yashita Namdeo <ynamdeo@measureone.com>
2022-04-30 20:25:31 +0530

    added basic auth layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,12 @@ const apiKey = "h8angzera3mz";
 
 const client = StreamChat.getInstance(apiKey);
 
+const authToken = false;
+
 const App = () => {
+
+  if(!authToken) return <Auth /> // hide everything if not logged in
+
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
---
+++ client/src/components/Auth.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import Cookies from 'universal-cookie';
+import axios from 'axios'; // TODO: npm install axios
+
+import signInImage from '../assets/signup.jpg';
+
+const Auth = () => {
+    const [isSignup, setIsSignup] = useState(false);
+
+    const handleChange = () => { }
+
+    const switchMode = () => {
+        setIsSignup((prevIsSignup) => !prevIsSignup);  // change state depending on previous state
+    }
+
+    return (
+        <div className="auth__form-container">
+            <div className="auth__form-container_fields">
+                <div className="auth__form-container_fields-content">
+                    <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
+                    <form onSubmit={() => { }}>
+                        {isSignup && ( // shorthand method for ternary operator (when we only need to show one thing based on this condition)
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="fullName">Full Name</label>
+                                <input
+                                    name="fullname"
+                                    type="text"
+                                    placeholder="Full Name"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor="username">Username</label>
+                            <input // we need username in signup and signin both
+                                name="username"
+                                type="text"
+                                placeholder="Username"
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="phoneNumber">Phone Number</label>
+                                <input
+                                    name="phoneNumber"
+                                    type="text"
+                                    placeholder="Phone Number"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="avatarURL">Avatar URL</label>
+                                <input
+                                    name="avatarURL"
+                                    type="text"
+                                    placeholder="Avatar URL"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor="password">Password</label>
+                            <input // we need password in signup and signin both
+                                name="password"
+                                type="password"
+                                placeholder="Password"
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="confirmPassword">Confirm Password</label>
+                                <input // we need confirm password only in signup
+                                    name="confirmPassword"
+                                    type="password"
+                                    placeholder="Confirm Password"
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </div>
+                        )}
+                    </form>
+                    <div className="auth__form-container_fields-account">
+                        <p>
+                            {isSignup ? "Already have an account?" : "Don't have an account?"}
+                            <span onClick={switchMode}>
+                                {isSignup ? "Sign In" : "Sign Up"}
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Auth
